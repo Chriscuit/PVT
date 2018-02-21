71afc7e1615edc45605a8cdeda0a4f5417c34cfa
Christopher R. Gill <chrisraygill@utexas.edu>
2018-02-21 15:20:32 -0600

    first commit

---
+++ app/src/main/java/com/humansignals/pvt/MainActivity.java
@@ -0,0 +1,20 @@
+package com.humansignals.pvt;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DrawingTheRectangle drawingTheRectangle = new DrawingTheRectangle(this);
+
+        setContentView(drawingTheRectangle);
+    }
+
+}
+
+
