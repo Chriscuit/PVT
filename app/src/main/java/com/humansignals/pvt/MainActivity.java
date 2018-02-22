5bbb8e5b6b208ff3ec3da24b87d086f1a2779dbc
Christopher R. Gill <chrisraygill@utexas.edu>
2018-02-22 05:43:03 -0600

    finished basic functionality

--- app/src/main/java/com/humansignals/pvt/MainActivity.java
+++ app/src/main/java/com/humansignals/pvt/MainActivity.java
@@ -1,20 +1,101 @@
 package com.humansignals.pvt;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Random;
+
+
+// make count box static size
+// make box disappear and reappear
+// make it change color with click
+// holder number longer with click
+// create intro/outro screen
+// let user choose when the process begins with initial button?
+// make app record other data?
+// add aspect of swiping in particular directions?
+// have stage #/# over the counter
+// have it make a sound on click
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView counter;
+    View button;
+
+    long init, now, time;
+    Handler handler;
+    Runnable updater;
+    int state = 0;
+
+    int min = 3000;
+    int max = 5000;
+    int rand;
+
+    int roundCap = 5;
+    int round = 0;
+
+    ArrayList<Long> data = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DrawingTheRectangle drawingTheRectangle = new DrawingTheRectangle(this);
+        button = (View) findViewById(R.id.button);
+        button.setOnClickListener(this);
+        counter = (TextView) findViewById(R.id.counter);
 
-        setContentView(drawingTheRectangle);
+        handler = new Handler();
+
+        updater = new Runnable() {
+            @Override
+            public void run() {
+                if (state == 0) {
+                    init = System.currentTimeMillis();
+                    state = 1;
+                }
+                now = System.currentTimeMillis();
+                time = now - init;
+                counter.setText("" + time);
+                handler.postDelayed(this, 30);
+
+                if (time > 15000) System.exit(0);
+            }
+        };
+
+        startRandCount();
     }
 
+    @Override
+    public void onClick(View v) {
+        round++;
+        data.add(time);
+
+        if (round == roundCap) {
+            System.exit(0);
+            saveData(data);
+        }
+        state = 0;
+        handler.removeCallbacks(updater);
+        counter.setText("");
+
+        startRandCount();
+    }
+
+    public void startRandCount() {
+        Random r = new Random();
+        rand = r.nextInt(max - min) + min;
+        handler.postDelayed(updater, rand);
+    }
+
+    public void saveData(ArrayList<Long> data) {
+        // do something here
+    }
 }
 
 
